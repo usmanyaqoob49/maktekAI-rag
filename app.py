48183fffda5bbc7121ebb83e2b1721178a476e2e
usmanyaqoob49 <usmanyaqoob49@gmail.com>
2024-11-19 16:39:02 +0500

    rag

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required, current_user
 from datetime import timedelta
+from rag import get_answer
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'helloworld123'  
